15663ef8bca12cdfc6070a42e8610aa23fc52868
Ivan <ivan.ingov@gmail.com>
2020-10-12 17:36:14 +0300

    Updates

---
+++ precache-manifest.610cc23683f6d2b5bc6792390e60d775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d1a9a26a984fdb787336d6883136a8",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "768c9ff727f57f382db4",
+    "url": "/counter/static/js/2.b7ed90ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.b7ed90ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69097b134223178eedea",
+    "url": "/counter/static/js/main.c1efe38a.chunk.js"
+  },
+  {
+    "revision": "e0ec91af1c389359ea90",
+    "url": "/counter/static/js/runtime-main.3e9d1e0a.js"
+  }
+]);
